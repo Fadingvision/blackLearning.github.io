b1f08ad35acdccf9458edf9c30164c1b0e2f5749
Yorke <541606115@qq.com>
2016-07-12 19:02:12 +0800

    Update you don't need jQuery.js

--- _posts/you don't need jQuery.js
+++ _posts/you don't need jQuery.js
@@ -20,4 +20,27 @@ el.previousElementSibling();
 el.nextElementSibling();
 
 // $('el').closest('queryString');
+function closest(el, selector) {
+    // matchesSelector用来匹配dom元素是否匹配某css selector。它为一些高级方法的实现提供了基础支持，比如事件代理，parent, closest等。
+    const matchsSelector = el.matchs || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+    // match start from el itself
+    while (el) {
+        if(matchsSelector.call(el, selector)) {
+            return el
+        }else {
+            el = el.parentElement;
+        }
+    }
+    return null;
+}
+
+/**
+ ***********************************
+  Form
+ ***********************************
+ */
+
+ // $('input').val();
+ document.querySelector('input').value;
 
